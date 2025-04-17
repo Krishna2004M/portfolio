3a33a3ad3754e9e68798b920c24e6bed61a02d58
Krishna2004M <krishbabu2004@gmail.com>
2025-04-17 22:00:07 +0530

    Updated some sections

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -42,12 +42,12 @@ export default function Greeting() {
                 <Button text="Contact me" href="#contact" />
                 {greeting.resumeLink && (
                   <a
-                    href={require("./resume.pdf")}
-                    download="Resume.pdf"
-                    className="download-link-button"
-                  >
-                    <Button text="Download my resume" />
-                  </a>
+                  href="/resume.pdf"
+                  download="Resume.pdf"
+                  className="download-link-button"
+                >
+                  <Button text="Download my resume" />
+                </a>                
                 )}
               </div>
             </div>
